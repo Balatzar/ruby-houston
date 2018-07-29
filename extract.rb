72ffa97f23db36e3571e0ea4c29d5001b8ceaac1
balatzar <bataprod@gmail.com>
2018-07-29 15:02:20 +0200

    Remove dev setup

--- extract.rb
+++ extract.rb
@@ -1,17 +1,7 @@
 require "csv"
-require "byebug"
-require 'open-uri'
+require "open-uri"
 
-# ouvrir le csv
-
-# parcourir chaque rangee
-# stocker la premiere cellule => nom de fichier
-# stocker la 3eme cellule => url a telecharger
-# telecharger le fichier et lui donner le bon nom
-
-LIMIT = 10
 destination = "./photos"
-i = 0
 
 Dir.mkdir(destination) unless Dir.exists?(destination)
 CSV.foreach("./images.csv", :headers => true) do |row|
@@ -21,6 +11,4 @@ CSV.foreach("./images.csv", :headers => true) do |row|
   File.open("#{destination}/#{name}", 'wb') do |fo|
     fo.write open(url).read
   end
-  break if i == LIMIT
-  i += 1
 end
