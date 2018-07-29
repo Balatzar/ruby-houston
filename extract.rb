e84ee0b42fdd6e4b3d9311f772ffa6906c742cd0
balatzar <bataprod@gmail.com>
2018-07-29 15:05:58 +0200

    Add image extension

--- extract.rb
+++ extract.rb
@@ -8,7 +8,7 @@ CSV.foreach("./images.csv", :headers => true) do |row|
   name = row[0]
   url = row[2]
   puts "Downloading #{name}"
-  File.open("#{destination}/#{name}", 'wb') do |fo|
+  File.open("#{destination}/#{name}.jpg", 'wb') do |fo|
     fo.write open(url).read
   end
 end
